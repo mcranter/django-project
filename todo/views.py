6bb156abe750a630ed72288641bc086ef2c03548
mcranter <mcranter@gmail.com>
2021-03-25 15:11:22 +0000

    templating added, formatted

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hi")
-
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
