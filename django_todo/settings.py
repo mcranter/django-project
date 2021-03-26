fc53d758bf6c50bc646a2722bc31526da6e126f0
mcranter <mcranter@gmail.com>
2021-03-26 13:32:07 +0000

    corrections

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -83,7 +83,7 @@ if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
